0ad87b013e1894a3daec860636caa932cf60cdc4
Jubaars <juliaalcassa692@gmail.com>
2024-11-18 22:24:32 -0300

    ex1

---
+++ ex1/script.js
@@ -0,0 +1,6 @@
+let materiaisEscolares = ["Caderno", "Borracha", "Canetinha", "Lapíz", "Tesoura", "Moxila", "Régua", "Apontador", "Marcador De Texto"];
+
+Produtos[3]="Lápis";
+Produtos[5]="Mochila";
+
+ console.log(materiaisEscolares);
